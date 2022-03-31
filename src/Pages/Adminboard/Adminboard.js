c46b46a2f398cb05a517654d0efa5a81f05f7b5a
caner <canerturkmen@msn.com>
2022-03-31 17:03:23 +0300

    login connected to backend

--- src/Pages/Adminboard/Adminboard.js
+++ src/Pages/Adminboard/Adminboard.js
@@ -1,10 +1,14 @@
 import Navbar from './../../Components/Navbar/Navbar';
 
 export default function Adminboard() {
+  if(localStorage.getItem('isLoggedIn') && localStorage.getItem('isLoggedIn') === 'true') {
   return (
     <div>
       <Navbar />
       <h1>Adminboard</h1>
     </div>
   );
+  } else {
+    window.location.href = '/login';
+}
 }
--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -3,7 +3,7 @@ import {ThemeProvider, createTheme} from '@mui/material/styles';
 import {Card} from '@mui/material';
 import {Typography} from '@mui/material';
 import Grid from '@mui/material/Grid';
-import { Button } from '@mui/material';
+import {Button} from '@mui/material';
 import './Profile.scss';
 
 const theme = createTheme({
@@ -25,44 +25,52 @@ const Profile = () => {
   const Role = 'Software Engineer';
   const Email = 'john@doe.com';
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
+  if (
+    localStorage.getItem('isLoggedIn') &&
+    localStorage.getItem('isLoggedIn') === 'true'
+  ) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Navbar />
 
-      <Grid container>
-        <Grid item xs={0} xl={1.5}></Grid>
-        <Grid item xs={12} xl={9}>
-          <Typography className="header" variant="h4" gutterBottom>
-            Profile
-          </Typography>
-          <Card className="profile-card" style={{backgroundColor: '#f2f2f2'}}>
-            <Typography className="p" variant="h5" gutterBottom>
-              Name: {Name}
+        <Grid container>
+          <Grid item xs={0} xl={1.5}></Grid>
+          <Grid item xs={12} xl={9}>
+            <Typography className="header" variant="h4" gutterBottom>
+              Profile
             </Typography>
-            <Typography className="p" variant="h5" gutterBottom>
-              Role: {Role}
-            </Typography>
-            <Typography className="p" variant="h5" gutterBottom>
-              Email: {Email}
-            </Typography>
-            <Button
-              sx={{mt: 1}}
-              className="button"
-              color="primary"
-              size="large"
-              variant="contained"
-              onClick={() => {
-                console.log('hello');
-              }}
-            >
-              Change Password
-            </Button>
-          </Card>
+            <Card className="profile-card" style={{backgroundColor: '#f2f2f2'}}>
+              <Typography className="p" variant="h5" gutterBottom>
+                Name: {Name}
+              </Typography>
+              <Typography className="p" variant="h5" gutterBottom>
+                Role: {Role}
+              </Typography>
+              <Typography className="p" variant="h5" gutterBottom>
+                Email: {Email}
+              </Typography>
+              <Button
+                sx={{mt: 1}}
+                className="button"
+                color="primary"
+                size="large"
+                variant="contained"
+                onClick={() => {
+                  console.log('hello');
+                }}
+              >
+                Change Password
+              </Button>
+            </Card>
+          </Grid>
+          <Grid item xs={0} xl={1.5}></Grid>
         </Grid>
-        <Grid item xs={0} xl={1.5}></Grid>
-      </Grid>
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+    );
+  } else {
+    window.location.href = '/login';
+    return <p> hello</p>;
+  }
 };
 
 export default Profile;
